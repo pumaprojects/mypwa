e71bf10d97d516a7085f5e75f82060f0beed9123
pumaprojects <pumaprojects@users.noreply.github.com>
2017-07-10 10:33:39 +0200

    inizio 10/07/2017

---
+++ scripts/app.js
@@ -0,0 +1,129 @@
+(function() {
+	'use strict';
+
+	var app = {
+		//visibleCards: {},
+		//daysOfWeek: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+		loader: document.querySelector('.loader'),
+		content: document.querySelector('#content'),
+		online: navigator.onLine
+	};
+
+
+	/*****************************************************************************
+	*
+	* Event listeners for UI elements
+	*
+	****************************************************************************/
+	
+	window.addEventListener('load', function() {
+		window.addEventListener('online',  app.update_online_status);
+		window.addEventListener('offline', app.update_online_status);
+		app.update_online_status();
+	});
+	
+	document.getElementById('but_refresh').addEventListener('click', function() {
+		app.load_stores();
+	});
+  
+	document.getElementById('btn_load').addEventListener('click', function() {
+		app.load_stores();
+	});
+  
+  
+app.update_online_status = function(){
+	app.online=navigator.onLine;
+	
+	if(app.online){
+		document.getElementById('online_text').innerHTML='on-line';
+		document.getElementById("online_indicator").className="online";
+	}else{
+		document.getElementById('online_text').innerHTML='off-line';
+		document.getElementById("online_indicator").className="offline";
+	}
+	
+	
+}
+  
+app.load_stores = function() {
+	//var url="https://127.0.0.1/mypwa_ws/services/get_punti_vendita";
+	//var url="http://localhost/mypwa_ws/services/get_punti_vendita";
+	var url="http://localhost/mypwa/data/stores.json";
+	
+	
+	
+	app.content.style.display="none"; 
+	app.loader.style.display="block"; 
+	
+	if(!app.online){
+		alert("dati da cache");
+		if ('caches' in window) {
+			//dati in cache
+			caches.match(url).then(function(response) {
+				if(response) {
+					response.json().then(function updateFromCache(json) {
+						app.show_stores(json)
+					});
+				}
+			});
+		}else{
+			//alert("no cache in window");
+		}
+	}else{
+		//alert("dati aggiornati");
+		// Fetch the latest data.
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+		  if (request.readyState === XMLHttpRequest.DONE) {
+			if (request.status === 200) {
+			  var response = JSON.parse(request.response);
+			  app.show_stores(response);
+			}
+		  } else {
+			// Return the initial weather forecast since no data is available.
+			//app.updateForecastCard(initialWeatherForecast);
+		  }
+		};
+		request.open('GET', url);
+		request.send();
+	}
+  }
+
+
+  app.show_stores = function(data) {
+	//alert(data[0].sto_descrizione);
+	var html='<table>';
+	html+='<tr>';
+	html+='<th>ID</th>';
+	html+='<th>Descrizione</th>';
+	html+='<th>Indirizzo</th>';
+	html+='<th>Cap</th>';
+	html+='<th>Città</th>';
+	html+='</tr>';
+    data.forEach(function(key) {
+      //alert(key.sto_descrizione);
+	  html+='<tr>';
+	  html+='<td>'+key.sto_id+'</td>';
+	  html+='<td>'+key.sto_descrizione+'</td>';
+	  html+='<td>'+key.sto_indirizzo+'</td>';
+	  html+='<td>'+key.sto_cap+'</td>';
+	  html+='<td>'+key.sto_citta+'</td>';
+	  html+='</tr>';
+    });
+	html+='</table>';
+	
+	app.content.innerHTML=html;
+	app.content.style.display="block"; 
+	app.loader.style.display="none"; 
+  }
+  
+  
+	if ('serviceWorker' in navigator) {
+		navigator.serviceWorker
+			.register('./service-worker.js')
+			.then(function() { console.log('Service Worker Registered'); });
+	}
+
+	app.update_online_status;
+	
+})();
---
+++ service-worker.js
@@ -0,0 +1,84 @@
+var dataCacheName = 'MyPwa-data';
+var cacheName = 'MyPwa';
+
+var filesToCache = [
+  '',
+  '/',
+  'manifest.json',
+  'index.html',
+  'scripts/app.js',
+  'styles/style.css',
+  'images/ic_add_white_24px.svg',
+  'images/ic_refresh_white_24px.svg'
+];
+
+//'offline-0.7.13/offline.min.js',
+//'offline-0.7.13/themes/offline-theme-chrome-indicator.css',
+//'offline-0.7.13/themes/offline-language-english-indicator.css'
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+		if (key !== cacheName && key !== dataCacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  
+  //var data_url="/mypwa_ws/services/";
+  var data_url="http://localhost/mypwa/data/stores.json";
+  
+  if (e.request.url.indexOf(data_url) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+	  
+	  if (e.request.url.indexOf('favicon.ico') <= -1) {
+		/*
+		 * The app is asking for app shell files. In this scenario the app uses the
+		 * "Cache, falling back to the network" offline strategy:
+		 * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+		 */
+		e.respondWith(
+		  caches.match(e.request).then(function(response) {
+			return response || fetch(e.request);
+		  })
+		);
+	  }
+  }
+});
